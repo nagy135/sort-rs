53d291b90eb8acf2dc23e58e8f61765e0565e567
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-06-02 21:48:14 +0200

    fix: iterate correct values

--- src/sorters.rs
+++ src/sorters.rs
@@ -4,8 +4,8 @@ impl Sorters {
     pub fn bubble_sort(data: &Vec<u32>) -> Vec<Vec<u32>> {
         let mut result: Vec<Vec<u32>> = vec![data.clone()];
         let mut copy = data.clone();
-        for _ in 0..data.len() - 1 {
-            for i in 0..data.len() - 1 {
+        for _ in 0..copy.len() - 1 {
+            for i in 0..copy.len() - 1 {
                 if copy[i] > copy[i + 1] {
                     let temp = copy[i];
                     copy[i] = copy[i + 1];
